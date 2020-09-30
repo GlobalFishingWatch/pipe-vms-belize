3fac4f426ee2d49d260fc2711ca3c0d155b774e1
Matias Piano <matias@globalfishingwatch.org>
2020-09-30 17:59:56 -0300

    Adds the Airflow Variable for the normalization DAG

--- airflow/post_install.sh
+++ airflow/post_install.sh
@@ -14,4 +14,16 @@ python $AIRFLOW_HOME/utils/set_default_variables.py \
     pipeline_dataset="{{ var.value.PIPELINE_DATASET }}" \
     belize_vms_gcs_path="gs://vms-gfw/belize/api" \
 
+python $AIRFLOW_HOME/utils/set_default_variables.py \
+    --force docker_image=$1 \
+    pipe_vms_belize \
+    dag_install_path="${THIS_SCRIPT_DIR}" \
+    dataflow_runner="DataflowRunner" \
+    docker_run="{{ var.value.DOCKER_RUN }}" \
+    project_id="{{ var.value.PROJECT_ID }}" \
+    temp_bucket="{{ var.value.TEMP_BUCKET }}"  \
+    pipeline_bucket="{{ var.value.PIPELINE_BUCKET }}" \
+    pipeline_dataset="{{ var.value.PIPELINE_DATASET }}" \
+    belize_vms_gcs_path="gs://vms-gfw/belize/api" \
+
 echo "Installation Complete"
